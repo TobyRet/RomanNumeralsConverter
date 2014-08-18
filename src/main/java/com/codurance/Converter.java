11703c234536424045e931e5f864fbc21cb92322
Toby Retallick <toby.retallick@gmail.com>
2014-08-18 10:59:03 +0100

    Extracted null argument test to its own method

--- src/main/java/com/codurance/Converter.java
+++ src/main/java/com/codurance/Converter.java
@@ -13,12 +13,22 @@ public class Converter {
     static {
         table = new TreeMap<Integer, String>(Collections.reverseOrder());
         table.put(1, "I");
+        table.put(4, "IV");
         table.put(5, "V");
+        table.put(9, "IX");
         table.put(10, "X");
+        table.put(40, "XL");
         table.put(50, "L");
+        table.put(90, "XC");
+        table.put(100, "C");
+        table.put(400, "CD");
+        table.put(500, "D");
+        table.put(900, "CM");
+        table.put(1000, "M");
     }
 
-    public String convert(int number) {
+    public String convert(Integer number) {
+        checkArgument(number);
         String romanNumeral = "";
         for (Map.Entry<Integer, String> entry : table.entrySet()) {
             Integer key = entry.getKey();
@@ -29,7 +39,13 @@ public class Converter {
                 romanNumeral += value;
             }
             number = remainder;
-        };
+        }
         return romanNumeral;
+    }
+
+    public void checkArgument(Integer number) {
+        if(number == null) {
+            throw new IllegalArgumentException("You did not enter a number");
+        }
     }
 }
